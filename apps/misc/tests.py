c26eadff4c71518dfa385ddadd7b54d7765679be
s13g <ayflix0@gmail.com>
2024-04-07 23:30:11 +0100

    feat(misc): wrote tests for misc app

--- apps/misc/tests.py
+++ apps/misc/tests.py
@@ -1,10 +1,11 @@
 import pyotp
 from django.contrib.auth import get_user_model
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from faker import Faker
 from rest_framework.test import APITestCase, APIClient
 
 from apps.core.models import OTPSecret
+from apps.misc.models import Tip
 
 User = get_user_model()
 
@@ -30,6 +31,10 @@ class TestCoreEndpoints(APITestCase):
         self.verify_email_url = reverse_lazy('verify-email')
         self.login_url = reverse_lazy('login')
         self.refresh_token_url = reverse_lazy('refresh-token')
+        self.retrieve_all_tips = reverse_lazy('retrieve-all-tips')
+        self.retrieve_single_tip = reverse_lazy('retrieve-tip')
+        self.filter_faqs = reverse_lazy('filter-all-faqs')
+        self.retrieve_faq_types = reverse_lazy('retrieve-all-faqs-types')
 
     def _employee_registration_success(self):
         # Test successful registration
@@ -120,3 +125,28 @@ class TestCoreEndpoints(APITestCase):
         tokens, _ = self._company_login_success()
         self.tokens = tokens
         self.client.force_authenticate(user=self.user.objects.get(), token=tokens.get('access'))
+
+    def test_retrieve_all_tips(self):
+        self._authenticate_with_tokens()
+        response = self.client.get(self.retrieve_all_tips)
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_single_tip(self):
+        self._authenticate_with_tokens()
+        # Create a new tip object
+        tip = Tip.objects.create(title='New tip', description='This is a new tip', author="John Doe")
+        new_tip_id = tip.id
+        # Use reverse when you are passing arguments
+        retrieve_single_tip_url = reverse('retrieve-tip', kwargs={'tip_id': new_tip_id})
+        response = self.client.get(retrieve_single_tip_url, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_filter_faqs(self):
+        self._authenticate_with_tokens()
+        response = self.client.get(self.filter_faqs)
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_faq_types(self):
+        self._authenticate_with_tokens()
+        response = self.client.get(self.retrieve_faq_types)
+        self.assertEqual(response.status_code, 200)
