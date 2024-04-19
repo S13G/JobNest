5f6a2b0c8eb09d63228c5dbe73b614a29c6f7024
s13g <ayflix0@gmail.com>
2024-04-19 08:53:53 +0100

    chore(pytest): changing some testcases to pytest fixtures where necessary

--- apps/jobs/tests.py
+++ apps/jobs/tests.py
@@ -23,17 +23,17 @@ class JobsTestCase(AuthTestCase):
             self.job_types_data = json.load(f)['job_types_data']
 
         self.job_types = JobType.objects.bulk_create([JobType(name=name) for name in self.job_types_data])
-
-        self.recruiter = User.objects.create_user(**self.recruiter_data, is_active=True, company=True)
+        # self.recruiter = User.objects.create_user(**self.recruiter_data, is_active=True, company=True)
+        # print(f'recruiter: {self.recruiter}')
 
         # read data from json file
         with open('apps/jobs/test_data.json', 'r') as f:
             self.jobs_data = json.load(f)['jobs_data']
 
         # Create database objects from supplied data
-        self.jobs = Job.objects.bulk_create([
-            Job(recruiter=self.recruiter, type=self.job_types[0], **job_data) for job_data in self.jobs_data
-        ])
+        # self.jobs = Job.objects.bulk_create([
+        #     Job(recruiter=self.recruiter, type=self.job_types[0], **job_data) for job_data in self.jobs_data
+        # ])
 
         # Store URLs in variables
         self.home_url = reverse_lazy('jobs-home')
